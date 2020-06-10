3a383cab79b11ec33d7384610e67882359998fa2
dungnx <dungnx@newwave.vn>
2020-06-10 14:22:57 +0700

    End of lesson 38

---
+++ MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Application.Interfaces;
+using MicroRabbit.Application.Services;
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Data.Repository;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterSevices(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Application Layer
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
+        }
+    }
+}
